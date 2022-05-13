aa291e7075e4a9965239d03c4c8fc6069542e4d6
Stephania Alexis <101305146+Babygirl110609@users.noreply.github.com>
2022-05-12 23:11:07 -0500

    inital commit with README.mg

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
-# Professional README Generator Starter Code
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+        
+# Title: node.js
+
+## Table Of Contents
+* [Description](#description)
+
+* [instruction](#instruction)
+
+* [usage](#usage)
+
+* [contribution](#contribution)
+
+* [email](#email)
+
+* [license](#license)
+
+* [github](#github)
+
+* [testing](#test)
+
+### Description
+this project is a friendly app to be able to reused components and front end libraries for future use.
+
+### Instruction
+using npm i to download the list that are in the package
+
+#### Usage
+node.js is is a faster and friendly tools that gives you access to file system and methosd of operating with files and other things that provides you a way to perform operations on your system
+
+###Testing
+npm test
+
+### Github
+[Github Profile](https://github.com/Babygirl110609)
+
+### License
+ ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+
+ ### Email
+ feel free to contact me if you have any questions mrslagos1592@yahoo.com
+        
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
 const inquirer = require("inquirer")
 // TODO: Create an array of questions for user input
 const questions = [
@@ -21,6 +22,11 @@ const questions = [
         type: "input",
         message: "Enter usage information",
         name: "usage"
+    },
+    {
+        type: "input",
+        message: "Enter testing instructions",
+        name: "test"
     },
     {
         type: "input",
@@ -55,6 +61,54 @@ function writeToFile(fileName, data) { }
 function init() { 
     inquirer.prompt(questions).then(response => {
         console.log(response)
+        let markdownContent = `
+        
+# Title: ${response.title}
+
+## Table Of Contents
+* [Description](#description)
+
+* [instruction](#instruction)
+
+* [usage](#usage)
+
+* [contribution](#contribution)
+
+* [email](#email)
+
+* [license](#license)
+
+* [github](#github)
+
+* [testing](#test)
+
+### Description
+${response.description}
+
+### Instruction
+${response.instruction}
+
+#### Usage
+${response.usage}
+
+###Testing
+${response.test}
+
+### Github
+[Github Profile](https://github.com/${response.github})
+
+### License
+ ![GitHub license](https://img.shields.io/badge/license-${response.license}-blue.svg)
+
+
+ ### Email
+ feel free to contact me if you have any questions ${response.email}
+        `
+fs.writeFileSync("README.md",markdownContent, function(err){
+    if(err)throw err;
+})
+        
+console.log("README generated")
     })
 }
 
